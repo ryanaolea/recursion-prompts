7c314b60de5a3589497e73b0be808226045b019d
Ryan Olea <ryanaolea@gmail.com>
2021-10-18 15:04:43 -0700

    implemented palindrome

--- src/recursion.js
+++ src/recursion.js
@@ -165,11 +165,27 @@ var reverse = function(string) {
         result += reverse(copy);
     }
     return result;
+
 };
 
 // 10. Write a function that determines if a string is a palindrome.
 var palindrome = function(string) {
-};
+    string = string.toLowerCase();
+    var first = string[0];
+    var last = string[string.length - 1];
+ 
+    var start = 1;
+    var end = string.length - 1;
+    var sames = first === last;
+ 
+    var copy = string.slice(start, end);
+ 
+    if (string.length > 2 && sames === true) {
+         palindrome(copy);
+    } 
+ 
+    return sames;
+ };
 
 /* ------------------------------------------------------------- */
 
